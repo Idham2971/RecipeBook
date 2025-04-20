95af8f65c9c2dd0125663ed4241298e9b821d2dc
Idham Khalid <idham290701@gmail.com>
2025-04-20 14:01:06 +0800

    mengubah layout dan menambahkan navbar pada bagian bawah

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,11 +32,16 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.android.material:material:1.10.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    // Glide untuk gambar
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
